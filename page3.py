39b33322465fb2712667aa1fae0e51c3abbbf738
khushraj17 <khushrajrane@gmail.com>
2026-07-03 22:57:24 +0530

    analytics page

---
+++ page3.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+
+st.set_page_config(
+    page_title="Analysis",
+    page_icon="📊",
+    layout="wide",
+    initial_sidebar_state="expanded",)
+
+st.markdown("""
+# Welcome to the Analysis Page! 📊""")
