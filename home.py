773d663849a2a13cacf7c086a4c441e0adf90ead
khushraj17 <khushrajrane@gmail.com>
2026-07-03 22:56:38 +0530

    home page of app

---
+++ home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Fundus App",
+    page_icon="🩺",
+    layout="wide",
+    initial_sidebar_state="expanded",)
+
+st.markdown("""
+# Welcome to the Fundus App!🩺""")
