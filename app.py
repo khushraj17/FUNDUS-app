97dc211dc6ba14feed6f61061af7263c514509e8
khushraj17 <khushrajrane@gmail.com>
2026-07-07 16:32:44 +0530

    add recommendation system

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 Home_page = st.Page("home.py", title="Home", icon="🏠")
 page_2 = st.Page("page2.py", title="Price Prediction", icon="📈")
 page_3 = st.Page("page3.py", title="Analysis", icon="📊")
+page_4 = st.Page("page4.py", title="Recommendation", icon="🏘️")
 
-
-pg = st.navigation([Home_page, page_2, page_3 ])
+pg = st.navigation([Home_page, page_2, page_3, page_4])
 pg.run()
