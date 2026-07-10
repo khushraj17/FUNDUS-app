b88fe0e92f93f3afa4e256bc7eae0e2214f0cc07
khushraj17 <khushrajrane@gmail.com>
2026-07-10 20:54:05 +0530

    shift the position of navigation bar

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import streamlit as st
+from component.ai_assistant import render_ai
 
-Home_page = st.Page("home.py", title="Home", icon="🏠")
-page_2 = st.Page("page2.py", title="Price Prediction", icon="📈")
-page_3 = st.Page("page3.py", title="Analysis", icon="📊")
-page_4 = st.Page("page4.py", title="Recommendation", icon="🏘️")
 
-pg = st.navigation([Home_page, page_2, page_3, page_4])
+st.set_page_config(
+    page_title="FUNDUS",
+    page_icon="🏡",
+    layout="wide"
+)
+
+home = st.Page("home.py", title="Home", icon="🏠")
+analytics = st.Page("page2.py", title="Price Prediction", icon="📈")
+prediction = st.Page("page3.py", title="Analysis", icon="📊")
+recommendation = st.Page("page4.py", title="Recommendation", icon="🏘️")
+
+pg = st.navigation(
+    {
+        "FUNDUS": [
+            home,
+            analytics,
+            prediction,
+            recommendation
+        ]
+    },
+    position="top" 
+)
+
 pg.run()
