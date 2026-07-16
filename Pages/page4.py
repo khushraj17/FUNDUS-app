41690f125591d7e01c7b031e143af18403e3ae60
khushraj17 <khushrajrane@gmail.com>
2026-07-16 09:40:03 +0530

    path changes

--- Pages/page4.py
+++ Pages/page4.py
@@ -12,11 +12,11 @@ st.set_page_config(
     initial_sidebar_state="expanded",)
 
 
-location_df = pickle.load(open("location_df.pkl",'rb'))
+location_df = pickle.load(open("data/pickle_files/location_df.pkl",'rb'))
 
-cosine_sim1 = pickle.load(open("cosine_sim1.pkl",'rb'))
-cosine_sim2 = pickle.load(open("cosine_sim2.pkl",'rb'))
-cosine_sim3 = pickle.load(open("cosine_sim3.pkl",'rb'))
+cosine_sim1 = pickle.load(open("data/pickle_files/cosine_sim1.pkl",'rb'))
+cosine_sim2 = pickle.load(open("data/pickle_files/cosine_sim2.pkl",'rb'))
+cosine_sim3 = pickle.load(open("data/pickle_files/cosine_sim3.pkl",'rb'))
 
 def recommend_properties_with_scores(property_name, top_n=247):
     
